908b0a825e397bec5634b25197e2689497e37464
jni-123 <73488943+jni-123@users.noreply.github.com>
2021-01-30 20:40:40 -0800

    Bug need to ask how to use reddit api

--- RedditTestMonkeyLearn.py
+++ RedditTestMonkeyLearn.py
@@ -7,11 +7,18 @@ def get_post(url):
     import comments
     reddit = comments.connect()
     comments = comments.get_comments(reddit, url)
-    print(comments)
+    #print(comments)
     return comments
 
+post = get_post("https://www.reddit.com/r/MouseReview/comments/jvvt6e/2020_is_the_year_i_discovered_actually_good_razer/")
 
+#print(post.keys)
+#separated_strings = seperate_into_strings(post[)
+'''
+entities = []
+for comment in post.keys():
+    doc = seperate_into_strings([comment])
 
-
-
-    
+    entities.append(doc)
+print(entities)
+'''
