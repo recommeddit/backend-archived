9e2c2efc65b3d78093b38d855e2ab5c828466d80
jni-123 <73488943+jni-123@users.noreply.github.com>
2021-01-30 18:27:15 -0800

    Updated. For next time Figure out how to factor in count, and relevance into whether or not the keywords are included

--- MonkeyLearnProductSentiment.py
+++ MonkeyLearnProductSentiment.py
@@ -7,7 +7,7 @@ api_key = os.getenv('URL_SCRAPE_API_KEY')
 ################################################################
 
 #temporary using api key, change this to .env later on
-ml = MonkeyLearn(URL_SCRAPE_API_KEY)
+ml = MonkeyLearn(api_key)
 
 
 def returnMonkey(data):
@@ -27,7 +27,42 @@ def returnPositiveorNot(data):
 
 def returnConfidence(data):
     #returns confidence in percentage
-    confidence = "{:.1%}".format(returnMonkey(data)['confidence'])
+    confidence = returnMonkey(data)['confidence']
     #confidence = "{:.1%}".format(0.999)
     return confidence
-print(returnConfidence(['laughing out loud']))
+#print(returnConfidence(['laughing out loud']))
+
+################################################################
+#seperate data into strings
+
+def seperate_into_strings(text):
+    #uses monkeylearn to extract OPINION units from text
+    #text is our input, a big reddit comment that may have multiple sentences
+    model_id = 'ex_N4aFcea3'
+    result = ml.extractors.extract(model_id, text)
+    #total is the extractions
+    total = result.body[0]['extractions']
+    #data is an array of parsed sentences of the original text
+    data = []
+    for i in range(len(total)):
+        data.append(total[i]['extracted_text'])
+    return data
+'''
+print(seperate_into_strings(['The hotel has a great location but all in all it was a horrible experience! \
+    Only stayed here because it was the pre-accomodation choice for one of our tours but it was terrible. \
+        Will never stay here again!'])[0])
+'''
+################################################################
+#THIS USES THE KEYWORD EXTRACTOR I DON'T KNOW HOW WELL THIS WILL WORKS
+#MAY HAVE A PROBLEM WITH DETECTING TOO MANY KEYWORDS
+#MAYBE I CAN INCLUDE VALUES WITH A CERTAIN AMOUNT OF RELEVANCE
+def keyword_extractor(data):
+    model_id = 'ex_YCya9nrn'
+    result = ml.extractors.extract(model_id, data)
+    array_of_keywords = []
+    #total is the extractions
+    total = result.body[0]['extractions']
+    for i in range(len(total)):
+        array_of_keywords.append(total[i]['parsed_value'])
+    return array_of_keywords
+
