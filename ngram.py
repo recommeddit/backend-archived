cfb1269af091712aeb04f691a6fcd24079884e4e
jni-123 <73488943+jni-123@users.noreply.github.com>
2021-05-19 22:36:09 -0700

    Added stopwords

--- ngram.py
+++ ngram.py
@@ -3,7 +3,23 @@ import pandas as pd
 import sklearn
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import confusion_matrix
+from nltk.corpus import stopwords 
 
+#removing stopwords(if but we he she)
+english_stop_words = stopwords.words('english')
+def remove_stop_words(corpus):
+    removed_stop_words = []
+    for review in corpus:
+        removed_stop_words.append(
+            ' '.join([word for word in review.split()
+                        if word not in english_stop_words]))
+    return removed_stop_words
+
+#print(remove_stop_words('bromwell high is a cartoon comedy'))
+
+def stemming():
+    return 0
+                        
 def split(inputstring):
     wordlist = inputstring.split()
     return wordlist
