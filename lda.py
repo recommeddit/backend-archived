1675c2e71c854eaab17482ede7617ef2e25e4e85
jni-123 <73488943+jni-123@users.noreply.github.com>
2021-05-21 00:47:32 -0700

    textpreprocessing completed

--- lda.py
+++ lda.py
@@ -5,6 +5,10 @@ from gensim.parsing.preprocessing import STOPWORDS
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
 from nltk.stem.porter import *
 import numpy as np
+import pandas as pd
+import nltk
+#nltk.download('wordnet')
+stemmer = SnowballStemmer('english')
 
 def lemmatize(text):
     return stemmer.stem(WordNetLemmatizer().lemmatize(text,pos = 'v'))
@@ -13,7 +17,13 @@ def preprocess(text):
     result = []
     for token in gensim.utils.simple_preprocess(text):
         if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 3:
-            result = append(lemmtize_stemming(token)),
+            result.append(lemmatize(token)),
     
     return result
 
+# example = 'I have to write this paper for this class'
+# words = []
+# for word in example.split(' '):
+#     words.append(word)
+# print(words)
+# print(preprocess(example))
