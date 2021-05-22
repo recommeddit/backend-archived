d796ffa963f6c244bd5e770d1fb1b8cce632a067
jni-123 <73488943+jni-123@users.noreply.github.com>
2021-05-21 22:42:24 -0700

    :)

--- nltk_test.py
+++ nltk_test.py
@@ -1,7 +1,11 @@
 import nltk
 import truecase
+from nltk.corpus import movie_reviews
+#nltk.download('punkt')
 
 sentence = 'michael jackson AnD His Dog like To eat At mcDonalds'
 text_to_tag = nltk.word_tokenize(truecase.get_true_case(sentence))
 
-print(nltk.pos_tag(text_to_tag))
+#print(nltk.pos_tag(text_to_tag))
+
+print(dir(movie_reviews))
