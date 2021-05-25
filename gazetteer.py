0221d2ecad3eba56131f7cc830c7dc7374365458
Gopu2001 <41317321+Gopu2001@users.noreply.github.com>
2021-05-25 12:39:27 -0700

    Unfinished Changes to gazetteer.py to fix some errors in the tree.has() function

--- gazetteer.py
+++ gazetteer.py
@@ -1,8 +1,8 @@
-import json, tqdm, sys
+import json, tqdm, sys, os
 import pandas as pd
 
 '''
-movies.tsv is a dataset downloaded from IMDB.
+mvnt.tsv is a dataset downloaded from IMDB.
 Only the following columns have been saved:
  * primaryTitle - most popular title
  * startYear    - start of release
@@ -15,8 +15,7 @@ gaz.dropna(subset=["title"], inplace=True)
 
 abbr_map = {
     "yrs" : "years",
-    "min" : "minutes",
-    "fuck" : "f"
+    "min" : "minutes"
 }
 
 # Testing the Affix Tree
@@ -25,6 +24,7 @@ abbr_map = {
 class Affix_tree:
     def __init__(self, loading_file=None):
         if loading_file != None:
+            print("Loading the Gazetteer")
             with open(loading_file) as file:
                 self.tree = json.loads(file.read())
         else:
@@ -43,9 +43,10 @@ class Affix_tree:
             sys.exit(1)
 
     def has(self, title):
+        title = title.lower()
         x = self.tree
         for character in title:
-            if character in x.keys():
+            if character.lower() in x.keys() or character.upper() in x.keys():
                 x = x[character]
             else:
                 return False
@@ -53,13 +54,34 @@ class Affix_tree:
             return True
 
     def save(self):
+        print("Saving the Gazetteer")
         with open("affix.tree", "w") as file:
             json.dump(self.tree, file)
 
+    def getMatchesInSentence(self, string):
+        matches = []
+        for start in range(len(string)):
+            x = self.tree
+            if string[start] in x.keys():
+                for next_char in range(len(string)-start):
+                    if string[next_char]:
+                        ...
+
+    def delete(self):
+        if os.path.exists("affix.tree"):
+            os.remove("affix.tree")
+            print("Successfully Deleted the Gazetteer")
+        else:
+            print("No 'affix.tree' file found in directory to delete.")
+
 if __name__ == "__main__":
-    tree = Affix_tree()
-    for title in tqdm.tqdm(gaz["title"]):
-        tree.add(title.lower())
-    tree.save()
+    # to create the affix.tree file
+    # tree = Affix_tree()
+    # tree.delete()
+    # print("Building the Gazetteer")
+    # for title in tqdm.tqdm(gaz["title"]):
+    #     tree.add(title.lower())
+    # tree.save()
     # to test loading...
-    # x = Affix_tree(loading_file = "affix.tree")
+    x = Affix_tree(loading_file = "affix.tree")
+    print(x.has("I"))
