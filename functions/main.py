e41a2d92b29a31e07d5107ac191fcd156beb86fc
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2021-10-17 15:20:22 -0700

    Comment out main function again

--- functions/main.py
+++ functions/main.py
@@ -12,19 +12,18 @@ def search(request):
 
     return results, 200, {'Access-Control-Allow-Origin': '*'}
 
-
-def main():
-    # assume first argument is query. Default query is 'C++ IDE'
-    try:
-        import sys
-        query = sys.argv[1]
-    except IndexError:
-        query = "similar movies to inception"
-
-    results = recommendations.get_recommendations(query)
-
-    print(results)
-
-
-if __name__ == "__main__":
-    main()
+# def main():
+#     # assume first argument is query. Default query is 'C++ IDE'
+#     try:
+#         import sys
+#         query = sys.argv[1]
+#     except IndexError:
+#         query = "similar movies to inception"
+#
+#     results = recommendations.get_recommendations(query)
+#
+#     print(results)
+#
+#
+# if __name__ == "__main__":
+#     main()
